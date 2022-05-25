b18c0469cc331b328669509ab1f31d20cedfdd6b
Thales Alves <tpereira@primeit.pt>
2022-05-25 23:38:57 +0100

    fixed validations for fields that cant be null and added indexes

--- src/main/kotlin/com/signicat/interview/adapters/data/entity/SubjectEntity.kt
+++ src/main/kotlin/com/signicat/interview/adapters/data/entity/SubjectEntity.kt
@@ -3,15 +3,26 @@ package com.signicat.interview.adapters.data.entity
 import com.signicat.interview.domain.model.UserGroup
 import java.util.*
 import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
-@Table(name = "subject")
+@Table(
+    name = "subject",
+    indexes = [
+        Index(name = "ix_subject_id", columnList = "id"),
+        Index(name = "ix_subject_username", columnList = "username")
+    ]
+)
 class SubjectEntity(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
+
+    @NotNull
     var username: String,
+
+    @NotNull
     var password: String,
 
     @ManyToMany(fetch = FetchType.EAGER, mappedBy = "subjects")
--- src/main/kotlin/com/signicat/interview/adapters/data/entity/UserGroupEntity.kt
+++ src/main/kotlin/com/signicat/interview/adapters/data/entity/UserGroupEntity.kt
@@ -2,14 +2,17 @@ package com.signicat.interview.adapters.data.entity
 
 import java.util.*
 import javax.persistence.*
+import javax.validation.constraints.NotNull
 
 @Entity
-@Table(name = "user_group")
+@Table(name = "user_group", indexes = [Index(name = "ix_user_group_id", columnList = "id")])
 class UserGroupEntity(
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long,
+
+    @NotNull
     var name: String,
 
     @ManyToMany(fetch = FetchType.EAGER)
